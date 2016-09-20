02d453bf8ea467951c1c7b43431c69e93399183d
Alexander Serkin <serkin.alexander@gmail.com>
2016-09-20 14:23:31 +0300

    Add Angular module name to layout

--- views/layouts/main.php
+++ views/layouts/main.php
@@ -13,7 +13,7 @@ AppAsset::register($this);
 ?>
 <?php $this->beginPage() ?>
 <!DOCTYPE html>
-<html lang="<?= Yii::$app->language ?>">
+<html lang="<?= Yii::$app->language ?> "ng-app="Skyeng"
 <head>
     <meta charset="<?= Yii::$app->charset ?>">
     <meta name="viewport" content="width=device-width, initial-scale=1">
